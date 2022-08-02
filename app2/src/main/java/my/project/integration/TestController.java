480f96c00242ed3b48150266877d729bb768c4c1
ksvelichko <ksvelichko@mts.ru>
2022-08-02 23:09:22 +0300

    request logging

---
+++ app2/src/main/java/my/project/integration/TestController.java
@@ -0,0 +1,13 @@
+package my.project.integration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String getIndexPage(){
+        return "Test";
+    }
+}
