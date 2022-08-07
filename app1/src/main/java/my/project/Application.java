fb263ca0061b3af245afacedefb6f20fbfcac378
ksvelichko <ksvelichko@mts.ru>
2022-08-07 16:37:53 +0300

    k8s services added

--- app1/src/main/java/my/project/Application.java
+++ app1/src/main/java/my/project/Application.java
@@ -1,10 +1,5 @@
 package my.project;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import my.project.integration.grpc.gen.TestRequest;
-import my.project.integration.grpc.gen.TestResponse;
-import my.project.integration.grpc.gen.TestServiceGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,15 +8,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9092)
-                .usePlaintext()
-                .build();
-
-        TestServiceGrpc.TestServiceBlockingStub stub = TestServiceGrpc.newBlockingStub(channel);
-        TestResponse testResponse = stub.test(TestRequest.newBuilder()
-                .setFirstName("Kirill")
-                .build());
-        System.out.println(testResponse);
-        channel.shutdown();
     }
 }
