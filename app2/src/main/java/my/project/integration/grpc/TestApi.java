ce93b79dd5e82e1618b60e68b5ffc1d95a543586
kvelichko <k.velichko@msn.com>
2022-12-06 21:48:03 +0300

    Project cleanup

--- app2/src/main/java/my/project/integration/grpc/TestApi.java
+++ app2/src/main/java/my/project/integration/grpc/TestApi.java
@@ -1,19 +1,19 @@
 package my.project.integration.grpc;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import my.project.integration.grpc.gen.TestRequest;
 import my.project.integration.grpc.gen.TestResponse;
 import my.project.integration.grpc.gen.TestServiceGrpc;
 import org.lognet.springboot.grpc.GRpcService;
 
 @GRpcService
+@Slf4j
 public class TestApi extends TestServiceGrpc.TestServiceImplBase {
 
     @Override
     public void test(TestRequest request, StreamObserver<TestResponse> responseObserver) {
-        System.out.println("App2: grpc test");
-        System.out.println(request.getFirstName());
-        System.out.println(request.getLastName());
+        log.info("Test request has received");
         TestResponse response = TestResponse.newBuilder()
                 .setGreeting("Hi " + request.getFirstName())
                 .build();
